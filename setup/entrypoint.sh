51c52e94340838d158eb7ff49c9fe47722aa4659
Dr. Safi <sfmissive@gmail.com>
2018-01-01 23:07:06 +0200

    Uploaded necessary files, Getting webapp to run.

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 currentRelativeFilePath=$(dirname "$0")
-echo host path: `pwd`/$currentRelativeFilePath/..
 # pwd - current working directory in host machine.
 # currentRelativeFilePath - path relative to where shell was executed from.
 # hostPath - will be used when calling docker-compose from inside 'manager' container to point to the host VM path rather than trying to mount from manager container. as mounting volumes from other container causes issues.
+applicationHostPath="`pwd`/$currentRelativeFilePath/.."
+echo host path: $applicationHostPath
+
 docker run \
-    --volume `pwd`/$currentRelativeFilePath/..:/project/application \
+    --volume $applicationHostPath:/project/application \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    --env "hostPath=`pwd`" \
+    --env "hostPath=$applicationHostPath" \
     myuserindocker/deployment-environment:latest \
     containerCommand "$@"
---
+++ source/serverSide/databaseDefaultData/webappSetting/template/documentFrontend.js
@@ -0,0 +1,102 @@
+let data = [
+    {
+        key: 'registration-single',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'registration-single',
+            file: 'registration-single.html'
+        }
+    },
+    {
+        key: 'registration-agency',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'registration-agency',
+            file: 'registration-agency.html'
+        }
+    },
+    {
+        key: 'homePage-view3',
+        layout: 'webapp-layout-list',
+        page: {
+            selectorName: 'view-view3',
+            file: 'view-view3.html'
+        }
+    },
+    {
+        key: 'homePage-view404',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'view-view404',
+            file: 'view-view404.html'
+        }
+    },
+    {
+        key: 'universityPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'universityPage',
+            file: 'view-underconstruction.html' 
+        }
+    },
+    {
+        key: 'studyfieldPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'studyfieldPage',
+            file: 'view-underconstruction.html' 
+        }
+    },
+    {
+        key: 'countryPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'countryPage',
+            file: 'view-list-item.html' 
+        }
+    },
+    {
+        key: 'bucharest',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'studyfieldSingleArticle',
+            file: 'view-article.html' 
+        }
+    },
+    {
+        key: 'medicine',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'studyfieldSingleArticle',
+            file: 'view-article.html' 
+        }
+    },
+    {
+        key: 'step',
+        layout: 'webapp-layout-step',
+        // page: {
+        //     selectorName: 'step1',
+        //     file: 'view-article.html' 
+        // }
+    },
+    {
+        key: 'frontpage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'frontPage',
+            file: 'view-frontpage.html' 
+        }
+    },
+    {
+        key: 'about',
+        layout: 'webapp-layout-toolbar',
+        page: {
+            selectorName: 'about',
+            file: 'view-about.html' 
+        }
+    },
+    
+]
+
+
+export default data
