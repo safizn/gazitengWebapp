a72b012373c668225d507144f6c806188db2a6ca
Safi <sfmissive@gmail.com>
2019-08-26 00:21:59 +0300

    ➕ Load additional app specific graph data.

--- source/serverSide/app.js
+++ source/serverSide/app.js
@@ -1,12 +1,11 @@
 import ownProjectConfig from '../../configuration'
 import { webappUserInterface } from '@dependency/appscript'
+import * as webappUIData from '../../resource/webappUI.graphData.json'
 
 // initialize services
 ;(async () => {
-  //TODO: load graph data and deal with NodeReferences
-
   console.groupCollapsed('• Run services:')
-  await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig })
+  await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig, additionalData: webappUIData })
   // await oAuthInitializePortServer()()
   // await openIdConnectInitializePortServer()()
   // await staticContentInitializePortServer({ entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd' })()
