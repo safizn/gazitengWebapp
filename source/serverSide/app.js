8cd35843496493b35f6168b193e9ec8925ec4915
Safi <sfmissive@gmail.com>
2019-08-28 15:12:35 +0300

    ✒ Import apiEndpoint service.

--- source/serverSide/app.js
+++ source/serverSide/app.js
@@ -1,15 +1,16 @@
 import ownProjectConfig from '../../configuration'
-import { webappUserInterface } from '@dependency/appscript'
-import * as webappUIData from '../../resource/webappUI.graphData.json'
+// import { webappUserInterface } from '@dependency/appscript'
+import { apiEndpoint } from '@dependency/appscript'
 
 // initialize services
 ;(async () => {
   console.groupCollapsed('• Run services:')
-  await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig, additionalData: webappUIData })
+  // await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig })
+  await apiEndpoint.initialize({ targetProjectConfig: ownProjectConfig })
+
   // await oAuthInitializePortServer()()
   // await openIdConnectInitializePortServer()()
   // await staticContentInitializePortServer({ entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd' })()
-  // await apiInitializePortServer()()
   // await websocketInitializePortServer()()
   console.groupEnd()
 
