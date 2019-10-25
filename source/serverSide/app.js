07e8f28d7ddfdac5ab2cd60efe49b75565d08170
Safi <nassarsafi@gmail.com>
2019-10-25 20:40:47 +0300

    ⚗ Refactor renaming files and updating appscript client api calls.

--- source/serverSide/app.js
+++ source/serverSide/app.js
@@ -1,17 +1,27 @@
 import ownProjectConfig from '../../configuration'
-// import { webappUserInterface } from '@dependency/appscript'
-import { apiEndpoint } from '@dependency/appscript'
+import {
+  apiEndpoint,
+  // oAuth,
+  // openIdConnect,
+  // staticContent,
+  // webappUserInterface,
+  // webSocket
+} from '@dependency/appscript'
+import { runDockerContainer } from '@dependency/deploymentScript/script/graphDatabase/memgraphContainer.js'
 
 // initialize services
 ;(async () => {
-  console.groupCollapsed('• Run services:')
-  // await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig })
-  await apiEndpoint.initialize({ targetProjectConfig: ownProjectConfig })
+  console.groupCollapsed('• Run prerequisite containers:')
+  runDockerContainer() // temporary solution
+  console.groupEnd()
 
-  // await oAuthInitializePortServer()()
-  // await openIdConnectInitializePortServer()()
-  // await staticContentInitializePortServer({ entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd' })()
-  // await websocketInitializePortServer()()
+  console.groupCollapsed('• Run services:')
+  await apiEndpoint.initialize({ targetProjectConfig: ownProjectConfig })
+  // await oAuth.initialize({ targetProjectConfig: ownProjectConfig })
+  // await openIdConnect.initialize({ targetProjectConfig: ownProjectConfig })
+  // await staticContent.initialize({ targetProjectConfig: ownProjectConfig, entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd' })
+  // await webappUserInterface.initialize({ targetProjectConfig: ownProjectConfig })
+  // await webSocket.initialize({ targetProjectConfig: ownProjectConfig })
   console.groupEnd()
 
   console.log('• WebApp up & running !')
